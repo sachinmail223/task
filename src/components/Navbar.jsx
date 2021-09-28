e354306036f5927d730b534778ff178ca8142927
sachin jain <sachinmail223@gmail.com>
2021-09-28 19:18:18 +0530

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import styles from './Navbar.module.css'
 const Navbar = () => {
     return (
         <div className={` ${styles.nav}`}>
-            <div className={`col-2 ${styles.logo}`}>
+            <div className={`col-3 ${styles.logo}`}>
               <p className={`${styles.logos1}`}>LOGO</p>
               <p className={`${styles.logos2}`}>Powered by CyberPal.io</p>
 
             </div>
-            <div className="col-2"><img src="./img/Group1649.png" alt="" /></div>
-            <div className="col-5"></div>
+            <div className="col-3"><img src="./img/Group1649.png" alt="" /></div>
+            <div className="col-2"></div>
             <div className="col-2">
                 <ul className={`${styles.ul}`}>
                     <li><a href="#">Company</a></li>
